bd80bc6dd20919923ea483dc167052f832ad28fd
B Salinas <brhythmic@gmail.com>
2012-09-24 00:25:34 -0700

    Initial Project creation commit

---
+++ MotionAudio/MotionAudio/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MotionAudio
+//
+//  Created by Brian Salinas on 9/24/12.
+//  Copyright (c) 2012 Bit Rhythmic Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MotionAudio/MotionAudio/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MotionAudio
+//
+//  Created by Brian Salinas on 9/24/12.
+//  Copyright (c) 2012 Bit Rhythmic Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
